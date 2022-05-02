424c2ea6627143ed5c809556431242c0aa29d8f2
fsociety <osama.shamout@lau.edu>
2022-05-02 05:04:33 +0300

    Modified Log In Page and miscs.

--- frontend/app/src/main/java/com/example/mypal/LogIn.java
+++ frontend/app/src/main/java/com/example/mypal/LogIn.java
@@ -2,7 +2,9 @@ package com.example.mypal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogIn extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void OnClickReturnToWelcome(View view){
+        Intent intent = new Intent(this,Welcome.class);
+        startActivity(intent);
     }
 }
