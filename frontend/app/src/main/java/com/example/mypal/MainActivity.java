f312cceb1c58ec698dcb43b32d0c5707f061d6bb
fsociety <osama.shamout@lau.edu>
2022-05-02 04:36:59 +0300

    Modified start button.

--- frontend/app/src/main/java/com/example/mypal/MainActivity.java
+++ frontend/app/src/main/java/com/example/mypal/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,14 +15,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //To make all elements fade in
+        com.airbnb.lottie.LottieAnimationView animation = findViewById(R.id.welcomeAnim);
+        TextView welcome_text = (TextView) findViewById(R.id.welcomeText);
+        Button start_button = (Button) findViewById(R.id.getStarted);
+
+       // animation.setX(-1000);
+      //  animation.animate().translationXBy(1000).rotation(3600).setDuration(1000);
+
+
+    }
+
+    public void OnClickGetStarted(View view){
         Intent intent = new Intent(this,LogIn.class);
         startActivity(intent);
-
-        //To make all elements fade in
-        com.airbnb.lottie.LottieAnimationView animation = findViewById(R.id.i);
-        TextView welcome_text = (TextView) findViewById(R.id.welcomeText);
-        Button start_button = (Button) findViewById(R.id.letsGetStarted);
-
-
     }
 }
