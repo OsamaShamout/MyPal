89dec61befee2d4e718260aade89f7358aba55a1
fsociety <osama.shamout@lau.edu>
2022-05-03 17:53:30 +0300

    Created activity page and a user profile page.

---
+++ frontend/app/src/main/java/com/example/mypal/ActivitiesPage.java
@@ -0,0 +1,16 @@
+package com.example.mypal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivitiesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activities_page);
+    }
+
+    
+}
