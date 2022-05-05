becd0a3a9c68901a62c64fcf86c465cd6258dc5d
fsociety <osama.shamout@lau.edu>
2022-05-05 05:25:51 +0300

    modified icons and homepage functions.

--- frontend/app/src/main/java/com/example/mypal/ActivitiesPage.java
+++ frontend/app/src/main/java/com/example/mypal/ActivitiesPage.java
@@ -105,5 +105,6 @@ public class ActivitiesPage extends AppCompatActivity {
             }
         });
 
+        share.show();
     }
 }
--- frontend/app/src/main/java/com/example/mypal/LogIn.java
+++ frontend/app/src/main/java/com/example/mypal/LogIn.java
@@ -3,16 +3,31 @@ package com.example.mypal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogIn extends AppCompatActivity {
 
-    @Override
+    EditText input_email;
+    EditText input_password;
+
+    String email_string;
+    String password_string;
+
+    Button log_in;
+
+    SharedPreferences sharedpref;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
+        input_email = (EditText) findViewById(R.id.editTextTextEmailAddressLogIn);
+        input_password = (EditText)  findViewById(R.id.editTextPasswordLogIn);
+        log_in = (Button) findViewById(R.id.loginButton);
 
     }
 
@@ -28,11 +43,16 @@ public class LogIn extends AppCompatActivity {
 
     boolean logged_in = false;
     public void OnClickLogIn(View view){
+        email_string = input_email.getText().toString();
+        password_string = input_password.getText().toString();
+
+        
 
         //Validate Information from DB
 
         logged_in = true;
-        //If true log in
+        //If true save e-mail to sharefpref.
+
         if(logged_in){
             Intent intent = new Intent(this,Homepage.class);
             startActivity(intent);
